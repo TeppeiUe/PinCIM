3870a6ff0187940d2a065e7d48d0be7c7401b5a5
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-05 14:52:02 +0000

    [add]sales_ends controller #5

---
+++ app/helpers/sales_ends_helper.rb
@@ -0,0 +1,2 @@
+module SalesEndsHelper
+end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :belongs, only: [:create, :index, :show, :edit, :update]
-  post 'belongs/search'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end

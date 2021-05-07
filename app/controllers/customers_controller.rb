261020b5c780e3b5eec8e7afbe111851e8d16015
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-07 23:28:44 +0000

    [add]customers controller #8

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,22 @@
+class CustomersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def search
+  end
+end

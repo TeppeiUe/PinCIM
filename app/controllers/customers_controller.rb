acf66338d8fdd2bb5e51babc5b26fdcf760e01c3
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-08 01:13:47 +0000

    [add]customers#new and #create #8

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,14 @@
 class CustomersController < ApplicationController
   def new
+    @customer = Customer.new
+    @key_people = KeyPerson.all
+    @sales_ends = SalesEnd.all
   end
 
   def create
+    @customer = Customer.new(params_customer)
+    @customer.save
+    redirect_to customers_path
   end
 
   def index
@@ -18,5 +24,16 @@ class CustomersController < ApplicationController
   end
 
   def search
+  end
+
+  private
+
+  def params_customer
+    params.require(:customer).permit(
+      :name,
+      :address,
+      :key_person_id,
+      :sales_end_id,
+    )
   end
 end

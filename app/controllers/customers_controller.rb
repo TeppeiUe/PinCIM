0f8aff2b52c45f3e8b844e51eb14c42457bc681c
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-08 03:19:56 +0000

    [update]customers controller by refactoring

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update]
+
   def new
     @customer = Customer.new
     @key_people = KeyPerson.all
@@ -21,17 +23,14 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def edit
-    @customer = Customer.find(params[:id])
     @key_people = KeyPerson.all
     @sales_ends = SalesEnd.all
   end
 
   def update
-    @customer = Customer.find(params[:id])
     if @customer.update(params_customer)
       redirect_to customer_path(@customer.id)
     else
@@ -49,6 +48,10 @@ class CustomersController < ApplicationController
   end
 
   private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 
   def params_customer
     params.require(:customer).permit(

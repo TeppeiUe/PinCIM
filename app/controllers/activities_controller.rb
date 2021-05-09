5605fc551f990f72fdb91a32620c1b8b2ed354fe
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-09 06:12:05 +0000

    [add]activities#index and #create #15

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,13 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.all
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(params_activity)
+    @activity.save
+    redirect_to activities_path
   end
 
   def edit
@@ -12,5 +17,12 @@ class ActivitiesController < ApplicationController
   end
 
   def search
+  end
+
+  private
+
+  def params_activity
+    params.require(:activity).permit(:name).
+      merge(category: params[:activity][:category].to_i)
   end
 end

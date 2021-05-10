24da099db3b1e5c54b159b8107c8fc1d2f5b2b0c
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-10 04:24:05 +0000

    [add]visit_records#new and #create #19

--- app/controllers/visit_records_controller.rb
+++ app/controllers/visit_records_controller.rb
@@ -1,8 +1,16 @@
 class VisitRecordsController < ApplicationController
   def new
+    @visit_record = VisitRecord.new
+    @customers = Customer.all
+    @key_people = KeyPerson.all
+    @belongs = Belong.all
+    @sales_ends = SalesEnd.all
   end
 
   def create
+    @visit_record = VisitRecord.new(params_visit_record)
+    @visit_record.save
+    redirect_to visit_record_path(@visit_record.id)
   end
 
   def index
@@ -21,5 +29,23 @@ class VisitRecordsController < ApplicationController
   end
 
   def search
+  end
+
+  private
+
+  def params_visit_record
+    params.
+      require(:visit_record).
+      permit(
+        :customer_id,
+        :key_person_id,
+        :belong_id,
+        :sales_end_id,
+        :visit_datetime,
+        :system,
+        :next_datetime,
+        :note,
+        :rank,
+      )
   end
 end

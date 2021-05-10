06e8779403ae6d2407530fa8475da9d7537f3e1f
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-10 05:39:41 +0000

    [add]visit_recordes#edit and #update #19

--- app/controllers/visit_records_controller.rb
+++ app/controllers/visit_records_controller.rb
@@ -22,9 +22,17 @@ class VisitRecordsController < ApplicationController
   end
 
   def edit
+    @visit_record = VisitRecord.find(params[:id])
+    @customers = Customer.all
+    @key_people = KeyPerson.all
+    @belongs = Belong.all
+    @sales_ends = SalesEnd.all
   end
 
   def update
+    @visit_record = VisitRecord.find(params[:id])
+    @visit_record.update(params_visit_record)
+    redirect_to visit_record_path(@visit_record.id)
   end
 
   def destroy

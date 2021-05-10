91060b8ae51d1f87efc465d797e32d3440745493
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-10 15:59:29 +0000

    [add]tasks#new and #create #21

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
   def new
+    @visit_record = VisitRecord.find(params[:visit_record_id])
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to visit_record_task_path(@task.visit_record_id, @task.id)
   end
 
   def index
@@ -15,5 +20,18 @@ class TasksController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def params_task
+    params.
+      require(:task).
+      permit(
+        :visit_record_id,
+        :title,
+        :content,
+        :deadline,
+      )
   end
 end

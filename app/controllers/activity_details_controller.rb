f736c6ec060db2e520fe3933a824821cd94e7f95
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-14 03:14:00 +0000

    [update]activity_details controller by refactoring

--- app/controllers/activity_details_controller.rb
+++ app/controllers/activity_details_controller.rb
@@ -1,7 +1,9 @@
 class ActivityDetailsController < ApplicationController
+  before_action :set_activity_detail_find, only: [:create, :update]
+  before_action :set_activity_detail, only: [:update, :destroy]
+
   def create
-    activity_detail_find = ActivityDetail.find_by(params_activity_detail)
-    if activity_detail_find.nil?
+    if @activity_detail_find.nil?
       @activity_detail = ActivityDetail.new(params_activity_detail)
       @activity_detail.save
     end
@@ -9,21 +11,26 @@ class ActivityDetailsController < ApplicationController
   end
 
   def update
-    activity_detail_find = ActivityDetail.find_by(params_activity_detail)
-    if activity_detail_find.nil?
-      @activity_detail = ActivityDetail.find(params[:id])
+    if @activity_detail_find.nil?
       @activity_detail.update(params_activity_detail)
     end
     redirect_to visit_record_path(params[:activity_detail][:visit_record_id])
   end
 
   def destroy
-    @activity_detail = ActivityDetail.find(params[:id])
     @activity_detail.destroy
     redirect_to visit_record_path(@activity_detail.visit_record_id)
   end
 
   private
+
+  def set_activity_detail
+    @activity_detail = ActivityDetail.find(params[:id])
+  end
+
+  def set_activity_detail_find
+    @activity_detail_find = ActivityDetail.find_by(params_activity_detail)
+  end
 
   def params_activity_detail
     params.require(:activity_detail).permit(:activity_id, :visit_record_id)

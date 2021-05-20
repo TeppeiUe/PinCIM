09509e888cf30c4dd21cf708ba15d7b7fa952f89
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-20 09:31:08 +0000

    [add]custom helper for text color #42

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,14 @@ module ApplicationHelper
   def btn_type(btn_name)
     btn_name.include?("変更") ? "primary" : "success"
   end
+
+  # タスクが実行中である場合緑色の文字
+  def status_color(active)
+    "text-success" if active
+  end
+
+  # タスクが期限切れでかつ実行中である場合赤文字
+  def deadline_color(active, deadline)
+    "text-danger" if active && deadline < Time.now
+  end
 end

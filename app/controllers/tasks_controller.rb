03eb142264dbb5752b93d0e265b01e93e652637a
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-28 07:25:44 +0000

    [update]time selection to step 15min by request

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,14 +4,24 @@ class TasksController < ApplicationController
   def new
     set_visit_record
     @task = Task.new
+    @deadline_time_nodefault = true
   end
 
   def create
+    @deadline_date = params_deadline[:deadline_date]
+    @deadline_time_hour = params_deadline["deadline_time(4i)"]
+    @deadline_time_minute = params_deadline["deadline_time(5i)"]
+
     @task = current_user.tasks.new(params_task)
+    # datetime型のデータに加工しレコードのdeadlimeカラムに追加
+    @task.deadline = datetime_join(@deadline_date, @deadline_time_hour, @deadline_time_minute)
+
     if @task.save
       render "create"
     else
       set_visit_record
+      # time_selectフォームににデータ反映するか判定
+      @deadline_time_nodefault = time_select_nodefault(@deadline_time_hour, @deadline_time_minute)
       render "new"
     end
   end
@@ -25,13 +35,31 @@ class TasksController < ApplicationController
 
   def edit
     set_visit_record
+    deadline = @task.deadline
+
+    # datetime型のデータをtime型やdate型に変換
+    @deadline_date = datetime_division(deadline, "datetime")
+    @deadline_time_hour = datetime_division(deadline, "time_hour")
+    @deadline_time_minute = datetime_division(deadline, "time_minute")
+
+    # time_selectフォームににデータ反映するか判定
+    @deadline_time_nodefault = time_select_nodefault(@deadline_time_hour, @deadline_time_minute)
   end
 
   def update
+    @deadline_date = params_deadline[:deadline_date]
+    @deadline_time_hour = params_deadline["deadline_time(4i)"]
+    @deadline_time_minute = params_deadline["deadline_time(5i)"]
+
+    # datetime型のデータに加工しレコードのdeadlimeカラムに追加
+    @task.deadline = datetime_join(@deadline_date, @deadline_time_hour, @deadline_time_minute)
+
     if @task.update(params_task)
       render "update"
     else
       set_visit_record
+      # time_selectフォームににデータ反映するか判定
+      @deadline_time_nodefault = time_select_nodefault(@deadline_time_hour, @deadline_time_minute)
       render "edit"
     end
   end
@@ -54,8 +82,11 @@ class TasksController < ApplicationController
         :visit_record_id,
         :title,
         :content,
-        :deadline,
         :is_active,
       )
+  end
+
+  def params_deadline
+    params.require(:task).permit(:deadline_date, "deadline_time(4i)", "deadline_time(5i)")
   end
 end

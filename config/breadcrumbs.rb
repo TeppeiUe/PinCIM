78efb88ae1e6b501db32119460a44d10dd81d466
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-05-31 08:04:11 +0000

    [add]gretel to tasks #63

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,9 +1,9 @@
 crumb :root do
-  link "Home", root_path
+  link "トップページ", root_path
 end
 
 crumb :visit_records do
-  link "訪問記録一覧", visit_records_path
+  link "訪問記録", visit_records_path
   parent :root
 end
 
@@ -20,4 +20,14 @@ end
 crumb :visit_records_counting do
 	link "訪問記録集計", counting_visit_records_path
 	parent :visit_records
+end
+
+crumb :tasks do
+  link "タスク", tasks_path
+  parent :root
+end
+
+crumb :task do |task|
+  link "【タスク】#{task.title}の詳細", visit_record_task_path(task.visit_record_id, task.id)
+  parent :tasks
 end

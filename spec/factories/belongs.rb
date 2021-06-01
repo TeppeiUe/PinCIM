f0dbf52f1c2ae53f679d1b0957731ce140263ed2
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-06-01 02:10:27 +0000

    [update]simple test code

--- spec/factories/belongs.rb
+++ spec/factories/belongs.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :belong do
     name { Faker::Lorem.characters(number: 10) }
-    address { Faker::Lorem.characters(number:10) }
+    address { Faker::Address.full_address }
     user
   end
 end
--- spec/models/belong_spec.rb
+++ spec/models/belong_spec.rb
@@ -2,23 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'Belongモデルのテスト', type: :model do
   let(:user) { create(:user) }
-  let!(:belong) { build(:belong, user_id: user.id) }
+  let!(:other_belong) { create(:belong, user_id: user.id) }
+  let(:belong) { build(:belong, user_id: user.id) }
 
   context 'validationのテスト' do
+    subject { belong.valid? }
+
     it 'nameカラムが空欄でないこと' do
       belong.name = ''
-      belong.valid?
-      expect(belong.errors.full_messages).to include("所属名を入力してください")
+      is_expected.to eq false
     end
 
     it 'nameカラムへの登録内容が重複しないこと' do
-      duplicate_belong = belong.dup
-      belong.save!
-      expect(duplicate_belong).to be_invalid
+      belong.name = other_belong.name
+      is_expected.to eq false
     end
   end
 
-  # 現aws環境ajax処理を含んだテストが出来ないため、以下を代替としてそれぞれ実施
+  # 現aws環境ajax処理を含んだテストが出来ないため、以下を代替として実施
   context '新規登録のテスト' do
     it '新規登録できること' do
       expect { belong.save }.to change(Belong, :count).by(1)
@@ -30,17 +31,23 @@ RSpec.describe 'Belongモデルのテスト', type: :model do
       belong.save
       @belong_old_name = belong.name
       @belong_old_address = belong.address
-      belong.name = Faker::Lorem.characters(number: 10)
-      belong.address = rand(0..3)
+
+      @belong_new_name = Faker::Lorem.characters(number: 10)
+      @belong_new_address = Faker::Address.full_address
+
+      belong.name = @belong_new_name
+      belong.address = @belong_new_address
       belong.save
     end
 
     it 'nameカラムが更新できる' do
       expect(belong.name).not_to eq @belong_old_name
+      expect(belong.name).to eq @belong_new_name
     end
 
     it 'addressカラムが更新できる' do
       expect(belong.address).not_to eq @belong_old_address
+      expect(belong.address).to eq @belong_new_address
     end
   end
 end

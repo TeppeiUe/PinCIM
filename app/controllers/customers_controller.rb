bdad83c6d970fa859e60b7909fafeebe799e8401
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-06-13 15:54:39 +0000

    [add]customer_key_people action to customers#new and #edit #81 -3

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,57 +12,81 @@ class CustomersController < ApplicationController
 
   def create
     @customer = current_user.customers.new(params_customer)
+    radio_key_person = params[:customer][:radio_key_person]
+    radio_sales_end = params[:customer][:radio_sales_end]
+    radio_belong = params[:customer][:radio_belong]
 
     @customer.transaction do
       # 窓口担当者で新規登録を選択
-      if params_key_person[:radio_key_person] == "new"
+      if radio_key_person == "new"
         @radio_key_person_select = ""
         @radio_key_person_new = "checked"
-        @key_person_new_value = params_key_person[:key_person_name]
+        @key_person_new_value = params[:customer][:key_person_name]
+
         key_person = current_user.key_people.create(name: @key_person_new_value)
+        flash.now[:alert_key_person] = key_person.errors.full_messages_for(:name).join
         @customer.key_person_id = key_person.id
       end
 
       # 営業担当者で新規登録を選択
-      if params_sales_end[:radio_sales_end] == "new"
+      if radio_sales_end == "new"
         @radio_sales_end_select = ""
         @radio_sales_end_new = "checked"
-        @sales_end_new_value = params_sales_end[:sales_end_name]
+        @sales_end_new_value = params[:customer][:sales_end_name]
+
         sales_end = current_user.sales_ends.new(name: @sales_end_new_value)
 
         # 所属で登録済を選択
-        if params_belong[:radio_belong] == "select"
-          if params_belong[:belong_id].empty?
-            flash.now[:alert_belong] = "所属を選択下さい"
-          else
-            @selected_belong = params_belong[:belong_id]
-            sales_end.belong_id = @selected_belong
-          end
+        if radio_belong == "select"
+          @selected_belong = params[:customer][:belong_id]
+
+          sales_end.belong_id = @selected_belong
+
         # 所属で新規登録を選択
         else
           @radio_belong_select = ""
           @radio_belong_new = "checked"
-          if params_belong[:belong_name].blank?
-            flash.now[:alert_belong] = "所属を入力下さい"
-          else
-            @belong_new_value = params_belong[:belong_name]
-            belong = current_user.belongs.create(name: @belong_new_value)
-            sales_end.belong_id = belong.id
-          end
+          @belong_new_value = params[:customer][:belong_name]
+
+          belong = current_user.belongs.create(name: @belong_new_value)
+          flash.now[:alert_belong] = belong.errors.full_messages_for(:name).join
+          sales_end.belong_id = belong.id
         end
         sales_end.save
+        flash.now[:alert_sales_end] = sales_end.errors.full_messages_for(:name).join
+        if radio_belong == "select"
+          flash.now[:alert_belong] =
+            sales_end.errors.full_messages_for(:belong).join
+        end
         @customer.sales_end_id = sales_end.id
       end
       @customer.save!
     end
+
+    # 窓口担当者の担当期間管理に新規登録
+    current_user.customer_key_people.create(
+      customer_id: @customer.id,
+      key_person_id: @customer.key_person_id
+    )
     redirect_to customer_path(@customer.id)
 
-    rescue => e
-      @key_people = current_user.key_people
-      @sales_ends = current_user.sales_ends
-      @belongs = current_user.belongs
-      gon.radio_sales_end_select = @radio_sales_end_select
-      render "new"
+  rescue => e
+    flash.now[:alert_name] = @customer.errors.full_messages_for(:name).join
+    if radio_key_person == "select"
+      flash.now[:alert_key_person] =
+        @customer.errors.full_messages_for(:key_person).join
+    end
+    if radio_sales_end == "select"
+      flash.now[:alert_sales_end] =
+        @customer.errors.full_messages_for(:sales_end).join
+    end
+
+    @key_people = current_user.key_people
+    @sales_ends = current_user.sales_ends
+    @belongs = current_user.belongs
+    gon.radio_sales_end_select = @radio_sales_end_select
+
+    render "new"
   end
 
   def index
@@ -75,7 +99,7 @@ class CustomersController < ApplicationController
   end
 
   def edit
-    @key_people = current_user.key_people
+    @key_people = KeyPerson.where(id: @customer.customer_key_people.pluck(:key_person_id))
     @sales_ends = current_user.sales_ends
   end
 
@@ -84,7 +108,7 @@ class CustomersController < ApplicationController
       @map_status = @customer.saved_change_to_attribute?(:address) ? "change" : "stay"
       render "update"
     else
-      @key_people = current_user.key_people
+      @key_people = KeyPerson.where(id: @customer.customer_key_people.pluck(:key_person_id))
       @sales_ends = current_user.sales_ends
       render "edit"
     end
@@ -132,28 +156,6 @@ class CustomersController < ApplicationController
       :longitude,
       :system,
       :note,
-    )
-  end
-
-  def params_key_person
-    params.require(:customer).permit(
-      :radio_key_person,
-      :key_person_name,
-    )
-  end
-
-  def params_sales_end
-    params.require(:customer).permit(
-      :radio_sales_end,
-      :sales_end_name,
-    )
-  end
-
-  def params_belong
-    params.require(:customer).permit(
-      :radio_belong,
-      :belong_id,
-      :belong_name,
     )
   end
 end

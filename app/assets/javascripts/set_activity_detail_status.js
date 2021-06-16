3e73103b9c2b30780b54c93566d7a4aadc0b0f62
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-06-16 03:49:34 +0000

    [add]javascript to change desplayed content #84

---
+++ app/assets/javascripts/set_activity_detail_status.js
@@ -0,0 +1,13 @@
+// 活動種別の登録の有無で表示を変更
+$(function(){
+  setActivityDetailStatus()
+});
+
+function setActivityDetailStatus() {
+  let count = $('ul.activity_detail_list').children().length;
+  if(!count) {
+    $('p.activity_detail_status').removeClass('d-none');
+  } else {
+    $('p.activity_detail_status').addClass('d-none');
+  }
+};

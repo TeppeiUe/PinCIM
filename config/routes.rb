6fdfc837fd5a3423e8f670a7ce1d410901d021b3
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-06-23 08:05:07 +0000

    [update]search path to have defined using collection by refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
     post '/sign_in' => 'devise/sessions#create'
     delete '/sign_out' => 'devise/sessions#destroy'
   end
+
   root 'homes#top'
   get '/map' => 'homes#map'
+  get '/tasks' => 'tasks#index'
+
   resources :visit_records do
     resources :tasks, only: [:new, :create, :show, :edit, :update, :destroy]
     resources :activity_details, only: [:create, :update, :destroy]
@@ -16,7 +19,7 @@ Rails.application.routes.draw do
       post 'search'
     end
   end
-  get '/tasks' => 'tasks#index'
+
   resources :activities, only: [:create, :index, :edit, :update]
   post 'activities/search'
 
@@ -29,14 +32,28 @@ Rails.application.routes.draw do
     end
   end
   resources :customers, only: [:new, :create, :index, :show, :edit, :update] do
+    collection do
+      post 'search'
+    end
     resources :customer_key_people, only: [:new, :create, :edit, :update, :destroy]
   end
-  post 'customers/search'
-  resources :key_people, only: [:create, :index, :show, :edit, :update]
-  post 'key_people/search'
-  resources :belongs, only: [:create, :index, :show, :edit, :update]
-  post 'belongs/search'
-  resources :sales_ends, only: [:create, :index, :show, :edit, :update]
-  post 'sales_ends/search'
+
+  resources :key_people, only: [:create, :index, :show, :edit, :update] do
+    collection do
+      post 'search'
+    end
+  end
+
+  resources :belongs, only: [:create, :index, :show, :edit, :update] do
+    collection do
+      post 'search'
+    end
+  end
+
+  resources :sales_ends, only: [:create, :index, :show, :edit, :update] do
+    collection do
+      post 'search'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
